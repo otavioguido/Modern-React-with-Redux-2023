b5bdffe1b3b165a416ac1aba7909c49006ca32e1
Otavio Guido Silva <otavio.guidosilva@corteva.com>
2023-04-20 08:55:13 -0300

    pdas project part 1

---
+++ pdas/src/ProfileCard.js
@@ -0,0 +1,14 @@
+function ProfileCard({ title, handle }) {
+    return (
+        <div>
+            <div>
+                Titles is {title}
+            </div>
+            <div>
+                Handle is {handle}
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
